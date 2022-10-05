201ee34823bd4df2dfbcd81d6958db9cd6a3f8be
Mahmoud Elbasiouny <elbasiouny.2@wright.edu>
2022-10-05 16:44:30 +0000

    Finished namechange script

---
+++ Lab05/namechange.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+printHelp() {
+	echo "Usage: namechange -f '"find"' -r '"replace"' '"file to modify"'"
+ 	echo "-f The text to find in the filename"
+ 	echo "-r The replacement text for the new filename"
+}
+
+while getopts ":hf:r:" opt; do
+	case $opt in
+	h)
+		printHelp
+		exit
+		;;
+	f)	
+		find_text=$OPTARG
+		;;
+	r) 	
+		replacement_text=$OPTARG
+		;;
+	\?)	
+		# Default case
+      	echo "Invalid option: -$OPTARG" >&2
+      	;;
+	esac
+done
+
+shift $((OPTIND-1))
+filename=$@
+
+# check if file is valid (if file exists, then execute, else print error and help guide)
+if [[ -e $filename ]]
+then
+	mv "$filename" "$(echo "$filename" | sed -e s/$find_text/$replacement_text/g)"
+else
+	echo "User must provide valid filename"
+	printHelp
+fi
