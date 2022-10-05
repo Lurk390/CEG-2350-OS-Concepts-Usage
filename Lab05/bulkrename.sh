4fef2817fdbd0a4158d8d751103c6ba3248084a4
Mahmoud Elbasiouny <elbasiouny.2@wright.edu>
2022-10-05 17:35:41 +0000

    FInished extra credit

---
+++ Lab05/bulkrename.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+printHelp() {
+	echo "Renames all files in current directory"
+	echo "Usage: bulkrename -f '"find"' -r '"replace""
+ 	echo "-f The text to find in the filenames"
+ 	echo "-r The replacement text for the new filenames"
+}
+
+while getopts ":hf:r:" opt; do
+	case $opt in
+	h)
+		printHelp
+		exit
+		;;
+	f)	
+		find_text=$OPTARG
+		;;
+	r) 	
+		replacement_text=$OPTARG
+		;;
+	\?)	
+		# Default case
+      	echo "Invalid option: -$OPTARG" >&2
+      	;;
+	esac
+done
+
+for item in *
+do
+	mv "$item" "$(echo "$item" | sed -e s/$find_text/$replacement_text/g)"
+done
